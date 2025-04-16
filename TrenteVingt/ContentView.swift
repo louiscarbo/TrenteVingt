d73b9f63b8ace374f2143dac774019694fb3e33c
louiscarbo <louis@carbo-estaque.fr>
2025-04-16 14:55:51 +0200

    Initial Commit

---
+++ TrenteVingt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrenteVingt
+//
+//  Created by Louis Carbo Estaque on 16/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrenteVingt/TrenteVingtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrenteVingtApp.swift
+//  TrenteVingt
+//
+//  Created by Louis Carbo Estaque on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrenteVingtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
